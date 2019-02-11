60b8693de86e22408443287f05c57320a0232f14
ivan <ivan@lanars.com>
2019-02-11 18:45:40 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_direct_select/dropdown_direct.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List _cities = [
+    "Cluj-Napoca",
+    "Bucuresti",
+    "Timisoara",
+    "Brasov",
+    "Constanta"
+  ];
+
+  List<DropdownDirectMenuItem<String>> _dropDownMenuItems;
+  String _currentCity;
+
+
+  @override
+  void initState() {
+    _dropDownMenuItems = getDropDownMenuItems();
+    _currentCity = _dropDownMenuItems[0].value;
+    super.initState();
+  }
+
+  List<DropdownDirectMenuItem<String>> getDropDownMenuItems() {
+    List<DropdownDirectMenuItem<String>> items = new List();
+    for (String city in _cities) {
+      items.add(new DropdownDirectMenuItem(value: city, child: new Text(city)));
+    }
+    return items;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Please choose your city: "),
+            new Container(
+              padding: new EdgeInsets.all(16.0),
+            ),
+            new DropdownDirectButton(
+              value: _currentCity,
+              items: _dropDownMenuItems,
+              onChanged: changedDropDownItem,
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.mail),
+            title: new Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Profile')
+          )
+        ],
+      ),
+    );
+  }
+
+  void changedDropDownItem(String selectedCity) {
+    setState(() {
+      _currentCity = selectedCity;
+    });
+  }
+}
